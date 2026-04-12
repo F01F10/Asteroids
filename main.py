22461373d7683f8d419141df152ce1c5c5ca9107
fiorafo1 <foivos.fioravantes@fit.cvut.cz>
2026-04-12 17:19:39 +0200

    Player is moving correctly

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    drawable= pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     #GAME LOOP
     while 1>0:
@@ -17,12 +24,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt/100)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt=pygame.time.get_ticks()/1000
-        
-
+    
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
